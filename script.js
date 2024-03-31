0cc3886236450d605f4b273c10fbf0963809a047
youcefM2002 <160131006+youcefM2002@users.noreply.github.com>
2024-03-31 15:58:01 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
-const sidebar = document.getElementById("sidebar");
-const main = document.getElementById("main");
 const toggleButton = document.getElementById("toggleButton");
+const sidebar = document.getElementById("sidebar");
 
 toggleButton.addEventListener("click", function() {
   sidebar.classList.toggle("open");
-  main.classList.toggle("shifted");
 });
-
-  z-index: 1001; /* Assurer que le bouton reste au-dessus du contenu et du sidebar */
-}
