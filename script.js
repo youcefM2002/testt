0571c8dc444eb4b7e97ed8b37e95ab4fe1fb2b8f
youcefM2002 <160131006+youcefM2002@users.noreply.github.com>
2024-03-31 16:11:36 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const toggleButton = document.getElementById("toggleButton");
 const sidebar = document.getElementById("sidebar");
+const content = document.getElementById("content");
 
 toggleButton.addEventListener("click", function() {
   sidebar.classList.toggle("open");
+  content.classList.toggle("shifted"); // Ajoute ou supprime la classe "shifted" sur le contenu principal
 });
