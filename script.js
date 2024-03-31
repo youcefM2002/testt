460ddbed73a37383512bd63ddcb8a9324e5d1e98
youcefM2002 <160131006+youcefM2002@users.noreply.github.com>
2024-03-31 15:53:16 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+const sidebar = document.getElementById("sidebar");
+const main = document.getElementById("main");
+const toggleButton = document.getElementById("toggleButton");
+
+toggleButton.addEventListener("click", function() {
+  sidebar.classList.toggle("open");
+  main.classList.toggle("shifted");
+});
 
